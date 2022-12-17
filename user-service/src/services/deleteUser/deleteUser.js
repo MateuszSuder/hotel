47ca63d85467c0abc3ae492a2ff51371f84b62b3
Mateusz Suder <matisu999@gmail.com>
2022-12-17 14:49:51 +0100

    Fix delete 404

--- user-service/src/services/deleteUser/deleteUser.js
+++ user-service/src/services/deleteUser/deleteUser.js
@@ -1,5 +1,6 @@
 import User from "../../schemas/User.js";
 import mongooseErrorResponse from "../../utils/mongooseErrorResponse.js";
+import genericErrorResponse from "../../utils/genericErrorResponse.js";
 
 /**
  * @param {e.Request} req
@@ -11,6 +12,7 @@ export default async (req, res) => {
 
     try {
         const result = await User.findOneAndUpdate({ _id: userId }, { isDeleted: true });
+        if(result === null) return genericErrorResponse(res, "Not found", 404);
 
         return res.status(200).send(null);
     } catch (e) {
