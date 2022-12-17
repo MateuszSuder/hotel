e31b3cdbf8ba479c6869caff052306add173cb82
Mateusz Suder <matisu999@gmail.com>
2022-12-17 15:14:10 +0100

    Add block user

--- user-service/src/server/routes/{userId}/block/userBlockRoute.js
+++ user-service/src/server/routes/{userId}/block/userBlockRoute.js
@@ -1,9 +1,21 @@
-import express from "express";
+import User from "../../../../schemas/User.js";
+import genericErrorResponse from "../../../../utils/genericErrorResponse.js";
+import mongooseErrorResponse from "../../../../utils/mongooseErrorResponse.js";
 
-const router = express.Router({ mergeParams: true });
+/**
+ * @param {e.Request} req
+ * @param {e.Response} res
+ */
+export default async (req, res) => {
+    // todo 403 implementation
+    const { userId } = req.params;
 
-router.post("", async (req, res) => {
-    res.send("User Block Post");
-});
+    try {
+        const result = await User.findOneAndUpdate({ _id: userId }, { isBlocked: true });
+        if(result === null) return genericErrorResponse(res, "Not found", 404);
 
-export default router
+        return res.status(200).send(null);
+    } catch (e) {
+        return mongooseErrorResponse(res, e);
+    }
+}
