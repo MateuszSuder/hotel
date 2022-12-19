5b93623a75ae949720ef64cfbd08ac4d94333ac9
Dawid Pers <dawid.g657@gmail.com>
2022-12-19 17:07:59 +0100

    Added error handler for roomTypeId not found

--- room-service/src/services/room/createRoom/createRoom.js
+++ room-service/src/services/room/createRoom/createRoom.js
@@ -1,4 +1,6 @@
 import Room from "../../../schemas/Room.js";
+import RoomType from "../../../schemas/RoomType.js";
+import genericErrorResponse from "../../../utils/genericErrorResponse.js";
 import mongooseErrorResponse from "../../../utils/mongooseErrorResponse.js";
 /**
  * @param {e.Request} req
@@ -7,14 +9,20 @@ import mongooseErrorResponse from "../../../utils/mongooseErrorResponse.js";
 export default async (req, res) => {
     try {
         const { roomTypeId, roomNumber, floor } = req.body;
+        const roomType = await RoomType.findById(roomTypeId);
 
+        if (!roomType)
+            return genericErrorResponse(
+                res,
+                `roomTypeId ${roomTypeId} not found`,
+                404
+            );
         const room = new Room({ roomTypeId, roomNumber, floor });
 
         const t = await room.save();
 
         res.status(201).json({ id: t.id });
     } catch (e) {
-        console.log(e.errors.roomTypeId);
         return mongooseErrorResponse(res, e);
     }
 };
