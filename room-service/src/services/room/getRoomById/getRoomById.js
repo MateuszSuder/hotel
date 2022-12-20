cf12f2a690a97df90d60f8d4d53035c5df0b98d9
Dawid Pers <dawid.g657@gmail.com>
2022-12-20 17:50:31 +0100

    fix return of getRoomById

--- room-service/src/services/room/getRoomById/getRoomById.js
+++ room-service/src/services/room/getRoomById/getRoomById.js
@@ -1,3 +1,4 @@
+import { ObjectID } from "bson";
 import Room from "../../../schemas/Room.js";
 import genericErrorResponse from "../../../utils/genericErrorResponse.js";
 import mongooseErrorResponse from "../../../utils/mongooseErrorResponse.js";
@@ -7,18 +8,48 @@ import mongooseErrorResponse from "../../../utils/mongooseErrorResponse.js";
  */
 export default async (req, res) => {
     const { roomId } = req.params;
-
     try {
-        const r = await Room.findOne({ _id: roomId }).populate("roomType");
-
-        if (!r)
+        const room = await Room.aggregate([
+            {
+                $match: {
+                    _id: ObjectID(roomId),
+                },
+            },
+            {
+                $lookup: {
+                    from: "roomtypes",
+                    localField: "roomType",
+                    foreignField: "_id",
+                    as: "roomType",
+                },
+            },
+            {
+                $unwind: {
+                    path: "$roomType",
+                },
+            },
+            {
+                $unset: "roomType._id",
+            },
+            {
+                $replaceRoot: {
+                    newRoot: {
+                        $mergeObjects: ["$$ROOT", "$roomType"],
+                    },
+                },
+            },
+            {
+                $unset: ["roomType", "__v"],
+            },
+        ]);
+        if (!room)
             return genericErrorResponse(
                 res,
                 `Room with id "${roomId}" not found`,
                 404
             );
 
-        res.status(200).json(r);
+        res.status(200).json(room);
     } catch (e) {
         return mongooseErrorResponse(res, e);
     }
