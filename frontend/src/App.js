a0c14de1223aaf48cc0cd0406173defee27cdd38
Dawid Pers <dawid.g657@gmail.com>
2022-12-28 20:34:09 +0100

    Create register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import RoomList from "./views/RoomList";
 import Profile from "./views/Profile";
 import Login from "./views/Login/Login";
 import React from "react";
+import Register from "./views/Login/Register";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route element={<Layout/>}>
-                  <Route path="/" element={<RoomList/>}/>
-                  <Route path="/profile" element={<Profile/>}/>
-              </Route>
-              <Route path="/login" element={<Login/>}/>
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<RoomList />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
