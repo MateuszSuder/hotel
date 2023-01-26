70939bd154d3b8197de8b06c40f4331b282c8778
Mateusz Suder <matisu999@gmail.com>
2023-01-26 23:58:37 +0100

    Adjust access to admin panel

--- frontend/src/views/AdminPanel/AdminPanel.js
+++ frontend/src/views/AdminPanel/AdminPanel.js
@@ -6,15 +6,25 @@ import PageWithMenu from "../../components/PageWithMenu/PageWithMenu";
 import AdminUsers from "../../components/AdminPanel/AdminUsers/AdminUsers";
 import AdminRoomTypes from "../../components/AdminPanel/AdminRoomTypes";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import useAuth from "../../context/AuthProvider";
+import {Navigate} from "react-router-dom";
+
 const subPages = [
     {name: "Użytkownicy", path: "users", icon: <People/>, component: AdminUsers},
-    {name: "Typy pokoi", path: "roomTypes", icon: <MeetingRoomIcon />, component: AdminRoomTypes,},
+    {name: "Typy pokoi", path: "roomTypes", icon: <MeetingRoomIcon />, component: AdminRoomTypes},
     {name: "Pokoje", path: "rooms", icon: <Bed/>, component: AdminRooms},
     {name: "Rezerwacje", path: "reservations", icon: <Bookmarks/>, component: AdminReservations},
 ]
 
-
 const AdminPanel = () => {
+    const { user } = useAuth();
+
+    if(user.role === "USER") {
+        return (
+            <Navigate to="/" />
+        )
+    }
+
     return (
         <PageWithMenu subPages={subPages}/>
     );
